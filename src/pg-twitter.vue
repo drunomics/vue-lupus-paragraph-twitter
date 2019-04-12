913384cd7fed42d1204039c526a64b46bd49257e
Maximilian Mikus <maximilianmikus@gmail.com>
2019-04-12 12:01:23 +0200

    LDP-160 Typo.

--- src/pg-twitter.vue
+++ src/pg-twitter.vue
@@ -22,7 +22,7 @@ export default {
     if (this.dataUrl !== '') {
       axios.get('https://publish.twitter.com/oembed?url=' + this.src, { adapter: jsonpAdapter }).then(response => {
         this.embedHtml = response.data.html
-        // The response HTML from instagram contains this script tag already,
+        // The response HTML from twitter contains this script tag already,
         // but via v-html it does not get executed so we need to add this element here manually.
         const script = document.createElement('script')
         script.src = '//platform.twitter.com/widgets.js'
